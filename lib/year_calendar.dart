0c1021dad372957b9bfd66be29cb2cc3a3e9ac87
alexeyismirnov <alexey@gmail.com>
2022-05-05 18:14:26 +0800

    year calendar legend

--- lib/year_calendar.dart
+++ lib/year_calendar.dart
@@ -6,6 +6,8 @@ import 'package:after_init/after_init.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 import 'month_view.dart';
+import 'globals.dart';
+import 'church_fasting.dart';
 
 class YearCalendarView extends StatefulWidget {
   final DateTime date;
@@ -16,17 +18,21 @@ class YearCalendarView extends StatefulWidget {
 }
 
 class _YearCalendarViewState extends State<YearCalendarView> with AfterInitMixin<YearCalendarView> {
-  late String title;
+  static const initialPage = 100000;
   late int initialYear;
 
+  late String title;
   late double cellWidth, containerWidth;
+  late PageController _controller;
 
   @override
   void initState() {
     super.initState();
 
     initialYear = widget.date.year;
-    title = "Календарь на 2022 г.";
+
+    _controller = PageController(initialPage: initialPage);
+    updateTitle();
   }
 
   @override
@@ -39,6 +45,26 @@ class _YearCalendarViewState extends State<YearCalendarView> with AfterInitMixin
       containerWidth = 300.0;
     }
   }
+
+  void updateTitle([int index = initialPage]) {
+    setState(() {
+      title = "year_calendar".tr().format([index - initialPage + initialYear]);
+    });
+  }
+
+  Widget getAppbar() => SliverAppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        floating: false,
+        pinned: false,
+        toolbarHeight: 50.0,
+        // actions: [],
+        title: AutoSizeText(title,
+            maxLines: 1,
+            minFontSize: 5,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline6),
+      );
 
   Widget getMonthView(DateTime d) => FittedBox(
           child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
@@ -61,30 +87,51 @@ class _YearCalendarViewState extends State<YearCalendarView> with AfterInitMixin
             decoration:
                 AppTheme.bg_decor_2() ?? BoxDecoration(color: Theme.of(context).canvasColor),
             child: SafeArea(
-                child: CustomScrollView(physics: const ClampingScrollPhysics(), slivers: [
-              SliverAppBar(
-                elevation: 0.0,
-                backgroundColor: Colors.transparent,
-                floating: false,
-                pinned: false,
-                toolbarHeight: 50.0,
-                // actions: [],
-                title: AutoSizeText(title,
-                    maxLines: 1,
-                    minFontSize: 5,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline6),
-              ),
-              SliverPadding(
-                  padding: const EdgeInsets.all(5),
-                  sliver: SliverGrid.count(
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 3,
-                      childAspectRatio: 1.0,
-                      children: List<int>.generate(12, (i) => i)
-                          .map<Widget>((i) => getMonthView(DateTime(initialYear, i + 1, 1)))
-                          .toList())),
-            ]))));
+                child: NestedScrollView(
+                    headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
+                        [getAppbar()],
+                    body: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        child: PageView.builder(
+                            controller: _controller,
+                            onPageChanged: (page) => updateTitle(page),
+                            itemBuilder: (BuildContext context, int index) => SingleChildScrollView(
+                                    child: Column(mainAxisSize: MainAxisSize.max, children: [
+                                  GridView.count(
+                                      shrinkWrap: true,
+                                      physics: const NeverScrollableScrollPhysics(),
+                                      crossAxisSpacing: 10,
+                                      mainAxisSpacing: 10,
+                                      crossAxisCount: 3,
+                                      childAspectRatio: 1.0,
+                                      children: List<int>.generate(12, (i) => i)
+                                          .map<Widget>((i) => getMonthView(DateTime(
+                                              index - initialPage + initialYear, i + 1, 1)))
+                                          .toList()),
+                                  const SizedBox(height: 20),
+                                  GridView.builder(
+                                      shrinkWrap: true,
+                                      physics: const NeverScrollableScrollPhysics(),
+                                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 3,
+                                        crossAxisSpacing: 10,
+                                        mainAxisSpacing: 10,
+                                        mainAxisExtent: 50, // here set custom Height You Want
+                                      ),
+                                      itemCount: FastingModel.types.length,
+                                      itemBuilder: (BuildContext context, int i) {
+                                        final t = FastingModel.types[i];
+
+                                        return Row(children: [
+                                          Container(width: 20, height: 20, color: t.color),
+                                          const SizedBox(width: 10),
+                                          Expanded(
+                                              child: AutoSizeText(t.description.tr(),
+                                                  maxLines: 2,
+                                                  minFontSize: 5,
+                                                  style: Theme.of(context).textTheme.subtitle1))
+                                        ]);
+                                      })
+                                ]))))))));
   }
 }
